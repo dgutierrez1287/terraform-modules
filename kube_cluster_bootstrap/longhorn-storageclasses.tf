c60d87767894659604c373ceaf7d5da661985253
Diego Gutierrez <dgutierrez1287@gmail.com>
2025-05-24 17:13:47 -0400

    added some longhorn storage classes

--- kube_cluster_bootstrap/longhorn-storageclasses.tf
+++ kube_cluster_bootstrap/longhorn-storageclasses.tf
@@ -3,3 +3,15 @@ resource "kubernetes_manifest" "longhorn_norep" {
     longhorn_namespace = var.longhorn_namespace  
   }))
 }
+
+resource "kubernetes_manifest" "longhorn_rwx" {
+  manifest = yamldecode(templatefile("${path.module}/templates/longhorn-rwx.yaml.tmpl", {
+    longhorn_namespace = var.longhorn_namespace
+  }))
+}
+
+resource "kubernetes_manifest" "longhorn_rwx_norep" {
+  manifest = yamldecode(templatefile("${path.module}/templates/longhorn-rwx-norep.yaml.tmpl", {
+    longhorn_namespace = var.longhorn_namespace
+  }))
+}
